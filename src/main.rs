a71ffa14214f3a215e401db91946108a153fd268
Zach Peters <zpeters@gmail.com>
2019-03-07 11:51:32 -0600

    setting up basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate clap;
+
+use clap::{Arg, App, AppSettings, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("speedtestr")
+        .version("0.0.1")
+        .about("Unofficial speedtest cli")
+        .author("Zach Peters")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .setting(AppSettings::ColoredHelp)
+        .subcommand(SubCommand::with_name("list").about("Lists available servers"))
+        .subcommand(SubCommand::with_name("ping")
+                    .about("Pings the best server")
+                    .arg(Arg::with_name("server")
+                         .short("s")
+                         .takes_value(true)
+                         .help("specify a server number to ping")))
+        .get_matches();
+
+    if app.is_present("list") {
+        println!("LSIT");
+    }
+
+    if let Some(app) = app.subcommand_matches("ping") {
+        println!("You chose ping");
+        println!("Server? {:#?}", app.is_present("server"));
+        println!("Server val {:#?}", app.value_of("server"));
+    }
+
 }
